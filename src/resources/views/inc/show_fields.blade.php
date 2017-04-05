b9230a2beb7eb8d078c71db7406504d63dd9c56e
Paul Glass-Steel <paul_gs@unc.edu>
2017-04-05 10:44:46 -0400

    Insert clearfix divs

--- src/resources/views/inc/show_fields.blade.php
+++ src/resources/views/inc/show_fields.blade.php
@@ -1,9 +1,16 @@
 {{-- Show the inputs --}}
 @foreach ($fields as $field)
+	@if( isset($field['grid_clearfix']) && $field['grid_clearfix'] == 'before' )
+    <div class="clearfix"></div>
+    @endif
     <!-- load the view from the application if it exists, otherwise load the one in the package -->
     @if(view()->exists('vendor.backpack.crud.fields.'.$field['type']))
         @include('vendor.backpack.crud.fields.'.$field['type'], array('field' => $field))
     @else
         @include('crud::fields.'.$field['type'], array('field' => $field))
+    @endif
+
+    @if( isset($field['grid_clearfix']) && $field['grid_clearfix'] == 'after' )
+    <div class="clearfix"></div>
     @endif
 @endforeach
